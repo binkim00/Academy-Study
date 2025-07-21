832550e2df771e85440a515efd52caf70fa2fd7f
binkim00 <k0b0301@naver.com>
2025-07-21 17:16:30 +0900

    2025-07-21. 학원 커밋

--- React/workspace/my-app/src/practice/Practice2.jsx
+++ React/workspace/my-app/src/practice/Practice2.jsx
@@ -1,70 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Practice2(props) {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const [pwTest, setPwTest] = useState("");
-  const [message, setMessage] = useState("빈 칸을 모두 입력하세요.");
-  const existingUsers = ["tester"];
-
-  const handleChangeId = (event) => setId(event.target.value);
-  const handleChangePw = (event) => setPw(event.target.value);
-  const handleChangePwTest = (event) => setPwTest(event.target.value);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (!id || !pw || !pwTest) {
-      setMessage("빈 칸을 모두 입력하세요.");
-      return;
+function Practice2(props){
+    const [user, setUser] = useState({id:"",pw:"", pwc:""});
+    const [message, setMessage] = useState("");
+    const changeUser = (event) =>{
+        // user객체에 변경된 데이터를 설정
+        // setState는 기존 객체를 변경하는것은 리렌더링되지 않음
+        // 새로운 객체를 만들어서 설정
+        // prev : 기존 데이터
+                      // { id:"",pw:"", pwc:"", id:value }
+        setUser((prev)=>({...prev, [event.target.name]:event.target.value}))
     }
-
-    if (pw !== pwTest) {
-      setMessage("비밀번호가 일치하지 않습니다.");
-      return;
+    useEffect(()=>{
+        if(user.id==="" || user.pw==="" || user.pwc === ""){
+            setMessage("빈 칸을 모두 입력하세요")
+        }else if(user.pw !== user.pwc){
+            setMessage("비밀번호가 일치하지 않습니다.");
+        }else if(user.id==="tester"){
+            setMessage("이미 존재하는 계정입니다.");
+        }else{
+            setMessage("");
+        }
+    },[user])
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(message === ""){
+            alert(`ID:${user.id} \r\nPassword1:${user.pw} \r\nPassword2:${user.pwc}`);
+        }else{
+            alert("작성한 내용을 확인해주세요.")
+        }
     }
-
-    if (existingUsers.includes(id)) {
-      setMessage("이미 존재하는 계정입니다.");
-      return;
-    }
-
-    setMessage("");
-    alert(`회원가입 성공!\r\nID: ${id}`);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>REACTERS</h1>
-      <h2>회원가입</h2>
-      <p>
-        <input
-          type="text"
-          value={id}
-          onChange={handleChangeId}
-          placeholder="아이디"
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          value={pw}
-          onChange={handleChangePw}
-          placeholder="비밀번호"
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          value={pwTest}
-          onChange={handleChangePwTest}
-          placeholder="비밀번호 확인"
-        />
-      </p>
-      <p>{message}</p>
-      <button type="submit">회원가입</button>
-    </form>
-  );
+    return(
+        <form onSubmit={handleSubmit}>
+            <h1>REACTERS</h1>
+            <h2>회원가입</h2>
+            <p><input type="text" name="id" value={user.id} onChange={changeUser} placeholder="아이디"/></p>
+            <p><input type="password" name="pw" value={user.pw} onChange={changeUser} placeholder="비밀번호"/></p>
+            <p><input type="password" name="pwc" value={user.pwc} onChange={changeUser} placeholder="비밀번호 확인"/></p>
+            <p>{message}</p>
+            <button>회원가입</button>
+        </form>
+    );
 }
-
 export default Practice2;
