e8a52f342475b4857e242b03a94252e40d2d329c
binkim00 <k0b0301@naver.com>
2025-07-29 17:39:43 +0900

    2025-07-29 학원 커밋

--- SpringBoot/workspace/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/service/UserService.java
+++ SpringBoot/workspace/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/service/UserService.java
@@ -11,12 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public Long save(AddUserRequest dto){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 // 비밀번호 저장시 암호화를 하여 저장, 암호화 하지 않으면 로그인 불가
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()
         ).getId();
     }
@@ -24,6 +25,11 @@ public class UserService {
     // Token에 저장하는 값이 user_id이기 때문에 작성하는 메서드
     public User findById(Long userId){
         return userRepository.findById(userId)
+                .orElseThrow(()->new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
                 .orElseThrow(()->new IllegalArgumentException("Unexpected user"));
     }
 }
